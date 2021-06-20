d3b7a8b310d164f34e9afdb82503733be18bfbbd
eyuta <shion612@gmail.com>
2021-06-21 06:38:57 +0900

    Add e2e

---
+++ e2e/cypress/integration/examples/sample.spec.ts
@@ -0,0 +1,8 @@
+context('google search', () => {
+  it('should perform basic google search', () => {
+    cy.visit('https://google.com');
+    cy.get('[name="q"]')
+      .type('subscribe')
+      .type('{enter}');
+  });
+});
