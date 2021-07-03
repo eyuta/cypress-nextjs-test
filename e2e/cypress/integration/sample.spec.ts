1fd876249bd0844d2987fc7477e9d1308f51fb94
eyuta <shion612@gmail.com>
2021-07-03 23:47:04 +0900

    add sample case

--- e2e/cypress/integration/sample.spec.ts
+++ e2e/cypress/integration/sample.spec.ts
@@ -17,3 +17,10 @@ describe('The Home Page', () => {
     cy.visit('/')
   })
 })
+context('Next.js test', () => {
+  it('should access localhost', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('h1')
+      .should('have.text', 'Welcome to Next.js!')
+  });
+});
